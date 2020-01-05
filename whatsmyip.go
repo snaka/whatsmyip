ea4ce32600dd15ecd03abd63964bddb66e9bd3a3
snaka <snaka.gml@gmail.com>
2020-01-05 15:26:20 +0900

    Discover public IP using ifconfig.me

--- whatsmyip.go
+++ whatsmyip.go
@@ -2,14 +2,20 @@ package whatsmyip
 
 import (
   "fmt"
+  "io/ioutil"
   "log"
+  "net/http"
   "runtime"
+
   "github.com/mbndr/figlet4go"
 )
 
-const version = "v0.1.0"
+const (
+  version = "v0.1.0"
+  discoverServiceURL = "https://ifconfig.me"
+)
 
-// ShowVersion shows version info
+// ShowVersion shows build version info
 func ShowVersion() {
   ascii := figlet4go.NewAsciiRender()
   versionStr, err := ascii.Render("what's my ip ?")
@@ -23,5 +29,13 @@ func ShowVersion() {
 
 // DiscoverPublicIP discovers public IP address of exected device
 func DiscoverPublicIP() string {
-  return "xxx.xxx.xxx.xxx"
+  res, err := http.Get(discoverServiceURL)
+  if err != nil {
+    log.Fatal(err)
+  }
+  publicIP, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  return string(publicIP)
 }
